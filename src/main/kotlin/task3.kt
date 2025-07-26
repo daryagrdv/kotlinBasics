822be4b5a9f0ff2eee6b451cec6923bd8e878af8
daryagordeeva <dgordeeva@hr-link.ru>
2025-07-26 19:39:55 +0300

    задача 3

---
+++ src/main/kotlin/task3.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val year: UInt = 1961u
+    var hour: UInt = 9u
+    var minute: UInt = 7u
+
+    println("Первый полет человека в космос приходится на $year год")
+    println("Старт (ч.) $hour")
+    println("Старт (мин.) $minute")
+
+    hour = 10u
+    minute = 55u
+
+    print("Посадка в ")
+    print("$hour")
+    print(":")
+    print("$minute")
+}
