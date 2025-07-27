15e49e2dfb4070c0cd9427007551b30b33c02e6c
daryagordeeva <dgordeeva@hr-link.ru>
2025-07-27 19:56:34 +0300

    Урок 2. Задача 2 - правки

--- src/main/kotlin/lesson2/task2.kt
+++ src/main/kotlin/lesson2/task2.kt
@@ -1,19 +1,19 @@
 package lesson2
 
+const val OLD_EMPLOYEES_COUNT: UInt = 50u
+const val OLD_EMPLOYEES_SALARY: UInt = 30000u
+
+const val TRAINEES_COUNT: UInt = 30u
+const val TRAINEES_SALARY: UInt = 20000u
+
 fun main(){
 
-    val oldEmployeesCount: UInt = 50u
-    val oldEmployeeSalary: UInt = 30000u
-
-    val traineesCount: UInt = 30u
-    val traineeSalary: UInt = 20000u
-
-    val oldEmployeesExpenses: UInt = oldEmployeeSalary * oldEmployeesCount
-    val traineesExpenses: UInt = traineeSalary * traineesCount
+    val oldEmployeesExpenses: UInt = OLD_EMPLOYEES_SALARY * OLD_EMPLOYEES_COUNT
+    val traineesExpenses: UInt = TRAINEES_SALARY * TRAINEES_COUNT
 
     val commonExpenses: UInt = oldEmployeesExpenses + traineesExpenses
 
-    val avrgEmployees: UInt = (oldEmployeesExpenses + traineesExpenses) / (oldEmployeesCount + traineesCount)
+    val avrgEmployees: UInt = (oldEmployeesExpenses + traineesExpenses) / (OLD_EMPLOYEES_COUNT + TRAINEES_COUNT)
 
     println("Затраты на старых сотрудников $oldEmployeesExpenses")
     println("Общие затраты на всех $commonExpenses")
