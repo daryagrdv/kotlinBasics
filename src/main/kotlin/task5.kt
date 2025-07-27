e485a4f8069f77e34d0c35c42007cc2b331304f8
daryagordeeva <dgordeeva@hr-link.ru>
2025-07-27 19:18:46 +0300

    task5* правки по ревью

--- src/main/kotlin/task5.kt
+++ src/main/kotlin/task5.kt
@@ -1,11 +1,14 @@
+const val MINUTE_AS_SECONDS = 60
+const val HOUR_AS_MINUTES = 60
+const val DURATION_IN_SECONDS = 6480
+
 fun main() {
-    val seconds: Short = 6480
 
-    val minutes: Int = seconds / 60
-    val secondsRest: Int = seconds % 60
+    val minutes: Int = DURATION_IN_SECONDS / MINUTE_AS_SECONDS
+    val secondsRest: Int = DURATION_IN_SECONDS % MINUTE_AS_SECONDS
 
-    val hours: Int = minutes / 60
-    val minutesRest: Int = minutes % 60
+    val hours: Int = minutes / HOUR_AS_MINUTES
+    val minutesRest: Int = minutes % HOUR_AS_MINUTES
 
     println(String.format("%02d:%02d:%02d", hours, minutesRest, secondsRest))
 }
