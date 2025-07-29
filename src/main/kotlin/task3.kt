6f2ea62c61b4ea156427c5d94a5adc4ae16eefb5
daryagordeeva <dgordeeva@hr-link.ru>
2025-07-29 22:41:25 +0300

    task3 ревью

--- src/main/kotlin/task3.kt
+++ src/main/kotlin/task3.kt
@@ -1,22 +1,24 @@
-const val YEAR = 1961
-const val FLIGHT_HOUR = "09"
-const val FLIGHT_MINUTE = "07"
-const val ARRIVAL_HOUR = "10"
-const val ARRIVAL_MINUTE = "55"
-
 fun main(){
-    var hour = FLIGHT_HOUR
-    var minute = FLIGHT_MINUTE
 
-    println("Первый полет человека в космос приходится на $YEAR год")
+    val year = 1961
+    val flightHour = "09"
+    val flightMinute = "07"
+    val arrivalHour = "10"
+    val arrivalMinute = "55"
+
+    var hour = flightHour
+    var minute = flightMinute
+
+    println("Первый полет человека в космос приходится на $year год")
     println("Старт (ч.) $hour")
     println("Старт (мин.) $minute")
 
-    hour = ARRIVAL_HOUR
-    minute = ARRIVAL_MINUTE
+    hour = arrivalHour
+    minute = arrivalMinute
 
     print("Посадка в ")
     print(hour)
     print(":")
     print(minute)
+
 }
